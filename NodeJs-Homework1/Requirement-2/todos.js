2ec60d5ee951ac576e852a3db484c7a129e14cac
Flamur Avdyli <145875910+Flamuravd@users.noreply.github.com>
2025-03-02 22:49:56 +0100

    Update todos.js

--- NodeJs-Homework1/Requirement-2/todos.js
+++ NodeJs-Homework1/Requirement-2/todos.js
@@ -1,21 +1,21 @@
 export const listOfTodos = [
   {
-    id: 1,
+    id: "1",
     isDone: false,
     title: "Buy milk"
   },
   {
-    id: 2,
+    id: "2",
     isDone: true,
     title: "Buy eggs"
   },
   {
-    id: 3,
+    id: "3",
     isDone: true,
     title: "Buy bread"
   },
   {
-    id: 4,
+    id: "4",
     isDone: false,
     title: "Buy butter"
   }
